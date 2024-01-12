bb85c04ca5b75bfad8b784e39b2e7c059c4343f5
satyam <satyamsingh17538@gmail,com>
2024-01-12 20:49:34 +0530

    1. modified in backend

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+
+const morgan = require("morgan");
+
+const bodyParser = require("body-parser");
+require("dotenv").config();
+var cors = require('cors');
+/*
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+
+
+
+// import routes
+const authRoutes = require('./routes/authRoutes');
+app.use('/api', authRoutes);
+
+//database connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log(err));
+
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+
+//ROUTES MIDDLEWARE
+// app.get('/', (req, res) => {
+//     res.send("Hello from Node Js");
+// })
+
+
+// error middleware
+app.use(errorHandler);
+
+//port
+*/
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
